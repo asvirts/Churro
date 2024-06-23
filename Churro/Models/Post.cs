c0675a64b9da60979f941f7d0b5a9571e5fb390c
Andrew Virts <asvirts@outlook.com>
2024-06-22 20:27:34 -0500

    supabase init

---
+++ Churro/Models/Post.cs
@@ -0,0 +1,20 @@
+﻿
+
+using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+
+namespace Churro.Models
+{
+	[Table("posts")]
+	class Post : BaseModel
+	{
+		[PrimaryKey("Id")]
+		public int Id { get; set; }
+
+		[Column("CreatedAt")]
+		public string CreatedAt { get; set; }
+
+		[Column("Title")]
+		public int Title { get; set; }
+	}
+}
