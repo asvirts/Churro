a1a03e6f20bfd3e96718d63c2d6ff4b496e837fc
Andrew Virts <asvirts@outlook.com>
2024-06-22 20:12:49 -0500

    Add project files.

---
+++ Churro/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Churro
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ Churro/MauiProgram.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Logging;
+using Supabase;
+
+namespace Churro
+{
+	public static class MauiProgram
+	{
+		public static MauiApp CreateMauiApp()
+		{
+			var builder = MauiApp.CreateBuilder();
+			builder
+				.UseMauiApp<App>()
+				.ConfigureFonts(fonts =>
+				{
+					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				});
+
+			//Supabase
+			var url = Environment.GetEnvironmentVariable("SUPABASE_URL");
+			var key = Environment.GetEnvironmentVariable("SUPABASE_KEY");
+
+			var options = new SupabaseOptions
+			{
+				AutoRefreshToken = true,
+				AutoConnectRealtime = true,
+				// SessionHandler = new SupabaseSessionHandler() <-- This must be implemented by the developer
+			};
+
+			// Note the creation as a singleton.
+			builder.Services.AddSingleton(provider => new Supabase.Client(url, key, options));
+
+
+			builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+			builder.Services.AddBlazorWebViewDeveloperTools();
+			builder.Logging.AddDebug();
+#endif
+
+			return builder.Build();
+		}
+	}
+}
